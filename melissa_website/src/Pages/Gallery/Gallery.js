695d661a5733b314e25e1a4829abbb008678c224
Bill-Wadsworth <Bill@wadsweb.com>
2023-09-17 16:16:42 +0100

    basic Gallery and test images

--- melissa_website/src/Pages/Gallery/Gallery.js
+++ melissa_website/src/Pages/Gallery/Gallery.js
@@ -1,3 +1,25 @@
+import GalleryViewModel from "./GalleryViewModel"
+
 export default function Gallery(){
-    return <p>Gallery</p>
+
+    const imagejson = GalleryViewModel()["Images"];
+
+    let numOfRows = 3;
+    
+
+    return (
+        <div className="flex">
+            { [...Array(numOfRows)].map((_, rowNum) => {
+                return(
+                    <div>
+                        {imagejson.filter((_, i) => {
+                            return (i+rowNum) % numOfRows === 0;
+                        }).map((i) => {
+                            return <img src={"./TestImages/"+i["image"]}></img>
+                        })}
+                    </div>
+                    )
+                })}
+        </div>
+    );
 }
---
+++ melissa_website/src/Pages/Gallery/GalleryViewModel.js
@@ -0,0 +1,8 @@
+import imagejson from "./images.json"
+
+export default function GalleryViewModel(){
+    
+    //replace with fetching from database
+    return imagejson
+
+}
