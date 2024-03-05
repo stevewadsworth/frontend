19ee35e23ec43069aba9c6405bb58c37a2ca02be
Steve Wadsworth <steve@wadsweb.com>
2024-03-05 20:54:21 +0000

    Fix the dates

--- src/Pages/Exhibitions/Event.tsx
+++ src/Pages/Exhibitions/Event.tsx
@@ -3,16 +3,24 @@ import React from "react";
 export default function Event({ eventInfo }){
 
     const formatDate = (date: string) => {
+        console.log(date)
         let theDate = new Date(date)
+        console.dir(theDate)
         return `${theDate.getDate()}/${theDate.getMonth() + 1}/${theDate.getFullYear()}`
     }
+
+    const oneDay = new Date(eventInfo.start).getTime() === new Date(eventInfo.end).getTime()
 
     return(
         <div className="m-8 font-light">
             <h2 className="mb-1 text-xl font-extralight">{eventInfo["title"]}</h2>
             <div className="text-sm">
                 <div className="mb-1">{eventInfo["description"]}</div>
-                <div>{formatDate(eventInfo["start"])} - {formatDate(eventInfo["start"])}</div>
+                { oneDay ? (
+                    <div>{formatDate(eventInfo.start)}</div>
+                ) : (
+                    <div>{formatDate(eventInfo.start)} - {formatDate(eventInfo.end)}</div>
+                )}
                 <div>{eventInfo["location"]}</div>
             </div>
         </div>
