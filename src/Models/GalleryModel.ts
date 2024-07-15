ddf1cbed5ebc4a2d2bc1b2c61355457be3fc9fe9
Steve Wadsworth <steve@wadsweb.com>
2024-07-15 21:45:31 +0100

    Progressive image loading

--- src/Models/GalleryModel.ts
+++ src/Models/GalleryModel.ts
@@ -8,15 +8,10 @@ export default async function GalleryModel(): Promise<Image[] | undefined> {
         // Read the JSON from storage
         const response = await firebase.fetchFromStorage('images/images.json')
         let galleryJson = await response.json() as Images
-        const galleryRaw = galleryJson?.Images
-        if (!galleryRaw) {
+        gallery = galleryJson?.Images
+        if (!gallery) {
             throw new Response("Not Found", { status: 404 })
         }
-        // The JSON contains image paths in the storage bucket so we need to convert the to URLs
-        gallery = await Promise.all(galleryRaw.map(async element => {
-            const url = await firebase.getURLForPath(`images/${element.image}`)
-            return { ...element, image: url }
-        }))
     } catch {
         throw new Response("Not Found", { status: 404 })
     }
---
+++ src/Pages/Gallery/ImageTile.tsx
@@ -0,0 +1,64 @@
+import { Link } from "react-router-dom";
+import { Image } from "../../Models/Image";
+import React, { useEffect, useRef, useState } from "react";
+import firebase from "../../firebase.ts";
+import SyncLoader from "react-spinners/SyncLoader"
+
+export default function ImageTile(props: { image: Image, index: number}) {
+    const [isLoading, setIsLoading] = useState(true);
+    const [imageURL, setImageURL] = useState('')
+    const placeholderRef = useRef(null);
+  
+    const imgRef = useRef<HTMLImageElement>(null);
+
+    useEffect(() => {
+        // Initiating Intersection Observer
+        const observer = new IntersectionObserver((entries) => {
+          // Set actual image source && unobserve when intersecting
+          if (entries[0].isIntersecting) {
+            firebase.getURLForPath(`images/${props.image.image}`).then((url) => {
+                setImageURL(url);
+            })
+
+            if (placeholderRef && placeholderRef.current) {
+                observer.unobserve(placeholderRef.current);
+            }
+          }
+        });
+    
+        // observe for an placeholder image
+        if (placeholderRef && placeholderRef.current) {
+          observer.observe(placeholderRef.current);
+        }
+      }, [props.image.image]);
+    
+
+    return (
+        <>
+          {isLoading && (
+            <div
+                ref={placeholderRef}
+                className="w-full aspect-square flex justify-center"
+            >
+                <div className="flex flex-col justify-center">
+                    <SyncLoader className="opacity-25" />
+                </div>
+            </div>
+          )}
+          {imageURL && (
+            <Link
+                to={"/gallery/" + props.index}
+                preventScrollReset={false}
+            >
+                <img
+                    ref={imgRef}
+                    src={imageURL}
+                    style={isLoading ? { display: "none" } : { display: "block" }}
+                    alt={props.image.title}
+                    onLoad={() => setIsLoading(false)}
+                />
+            </Link>
+          )}
+        </>
+      )
+}
