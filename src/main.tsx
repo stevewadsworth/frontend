99f504059907bea48b629b8df492d5dad027d98b
Bill-Wadsworth <Bill@wadsweb.com>
2024-08-12 17:45:28 +0100

    removed report web vitals

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './main.css';
-//import reportWebVitals from './reportWebVitals.ts';
 
 import { createBrowserRouter, RouterProvider, Navigate } from "react-router-dom";
 
@@ -60,10 +59,6 @@ if (rootEl) {
     </React.StrictMode>
   );
 
-  // If you want to start measuring performance in your app, pass a function
-  // to log results (for example: reportWebVitals(console.log))
-  // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-  //reportWebVitals();
 } else {
   console.error("NO ROOT ELEMENT FOUND!")
 }
